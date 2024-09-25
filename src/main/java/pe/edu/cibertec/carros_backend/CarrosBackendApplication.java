3437a2aecdbc966c517a6b1c35408f494f511c76
DAVID <fernandez.davidgoza@gmail.com>
2024-09-24 22:07:00 -0500

    carga inicial proyecto carros backend

---
+++ src/main/java/pe/edu/cibertec/carros_backend/CarrosBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.carros_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/carros_backend/CarrosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.carros_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
